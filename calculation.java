fd7a51030e4d45ba267e0584fb44ac2c8205d30e
shrikant2727 <91520838+shrikant2727@users.noreply.github.com>
2021-09-28 12:22:01 +0530

    shrikant

---
+++ calculation.java
@@ -0,0 +1,10 @@
+package math1;
+
+public class calculation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println(" hello\n shrikant patil");
+	}
+
+}
